0fcd8223b405887e0e9d3ac15ad9547335ae1913
White-prince <tiltedfear@gmail.com>
2022-08-14 01:40:41 +0300

    Add navigation

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-
+    path('about', views.homepage, name='about'),
+    path('blog', views.homepage, name='blog'),
+    path('contact', views.homepage, name='contact'),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
