2fa05e7b3b1da8d8f975ad9b263f582398ff439a
White-prince <tiltedfear@gmail.com>
2022-08-15 22:30:14 +0300

    edit work blocks and form

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from taskmanager.settings import RECIPIENTS_EMAIL, DEFAULT_FROM_EMAIL
 
 
 def homepage(request):
@@ -14,8 +18,22 @@ def blog(request):
 
 
 def contact(request):
-    return render(request, 'main/contact.html')
-
+    if request.method == 'GET':
+        form = ContactForm()
+    elif request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['name']
+            sender = form.cleaned_data['sender']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, f'{message} от {sender}', DEFAULT_FROM_EMAIL, RECIPIENTS_EMAIL)
+            except BadHeaderError:
+                return HttpResponse('Ошибка в теме письма.')
+            return HttpResponseRedirect('contact')
+    else:
+        return HttpResponse('Неверный запрос.')
+    return render(request, 'main/contact.html', {'form': form})
 
 def vedmenkoproduction(request):
     return render(request, 'main/vedmenko.html')
